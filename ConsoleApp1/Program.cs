11e769ccdbe7f99eb75a45d50737244334ee55df
abdullapro2006 <abdullahmanafli38@gmail.com>
2023-07-10 15:06:21 +0400

    the first task completed

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Globalization;
+
+public class Program 
+{
+    public static void Main(string[] args)
+    {
+        string sentence = "CodeAcademy is a good place to study";
+        string[] result = sentence .Split(' ');
+        Console.WriteLine($"Total number of words in the string is:{result.Length}");
+
+
+    }
+}
+
